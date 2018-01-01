59d8d880c9de0311d36d143d88043d02488f7724
RichardLaney <richardjlaney@gmail.com>
2018-01-01 14:32:37 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+bot.user.setStatus('Doki Doki Literature Club')
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
