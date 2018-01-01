4a86131b64c1ccfd2dbad2dee8ed4049e6363388
RichardLaney <richardjlaney@gmail.com>
2018-01-01 14:46:06 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 client.on('ready', () => {
-	
+    console.log('I am ready!');
+
 	bot.user.setStatus('dnd')
 	
 	bot.user.setGame('Doki Doki Literature Club')
-});	
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+	
+	});
 
 client.on('message', message => {
     if (message.content === 'ping') {
