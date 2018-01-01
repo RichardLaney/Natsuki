396edc81e40b297bc7506dac252c1144f72724c8
RichardLaney <richardjlaney@gmail.com>
2018-01-01 14:37:31 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+bot.user.setStatus('Doki Doki Literature Club')
 client.on('ready', () => {
     console.log('I am ready!');
 });
